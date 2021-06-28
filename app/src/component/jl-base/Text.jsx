e6667ba13d6020c0bcc527dd6d3ce01eab18b29b
laststory117 <1172002409@qq.com>
2021-06-28 22:59:52 +0800

    文字样式支持编辑

--- app/src/component/jl-base/Text.jsx
+++ app/src/component/jl-base/Text.jsx
@@ -1,6 +1,18 @@
 export default {
   name: 'JlText',
+  props: {
+    config: { type: Object, default: () => {} }
+  },
   render() {
-    return <span>一段文字</span>;
+    const { style, text } = this.config;
+    const { width, height, fontSize, lineHeight } = style;
+    const resultStyle = {
+      ...style,
+      width: `${width}px`,
+      height: `${height}px`,
+      lineHeight: `${lineHeight}px`,
+      fontSize: `${fontSize}px`
+    };
+    return <p style={resultStyle}>{text}</p>;
   }
 };
--- app/src/component/jl-editor/TextEditor.vue
+++ app/src/component/jl-editor/TextEditor.vue
@@ -2,19 +2,29 @@
   <div>
     <el-form :model="config" size="small" :inline="true">
       <el-form-item label="文字内容">
-        <el-input v-model="config.text"></el-input>
+        <el-input v-model="config.text" />
       </el-form-item>
       <el-form-item label="文字颜色">
-        <el-color-picker
-          v-model="config.style.color"
-          show-alpha
-        ></el-color-picker>
+        <el-color-picker v-model="config.style.color" show-alpha />
       </el-form-item>
       <el-form-item label="字体大小">
         <el-input-number
           v-model="config.style.fontSize"
           controls-position="right"
-        ></el-input-number>
+        />
+      </el-form-item>
+      <el-form-item label="文字行高">
+        <el-input-number
+          v-model="config.style.lineHeight"
+          controls-position="right"
+        />
+      </el-form-item>
+      <el-form-item label="对齐方式">
+        <el-radio-group v-model="config.style.textAlign" size="mini">
+          <el-radio-button label="left" name="ad"></el-radio-button>
+          <el-radio-button label="center"></el-radio-button>
+          <el-radio-button label="right"></el-radio-button>
+        </el-radio-group>
       </el-form-item>
     </el-form>
   </div>
