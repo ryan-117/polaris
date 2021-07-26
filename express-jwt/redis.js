f987b8cbea33e977a6a6cb616d9c5fe660750f9b
60617 <yurong.pyr@alibaba-inc.com>
2021-07-26 09:51:53 +0800

    读写redis

---
+++ express-jwt/redis.js
@@ -0,0 +1,33 @@
+// 读写redis
+var config = require('./config');
+var redis = require('redis');
+var client = redis.createClient(6379, config.redislink);
+
+var setkv = function setkv(key, value, expire) {
+  return new Promise(resolve => {
+    client.set(key, value, 'EX', expire, function (err, reply) {
+      if (err) {
+        resolve(null);
+      } else {
+        resolve(reply);
+      }
+    });
+  });
+};
+
+var getkey = function getkey(key) {
+  return new Promise(resolve => {
+    client.get(key, function (err, reply) {
+      if (err) {
+        resolve(null);
+      } else {
+        resolve(reply);
+      }
+    });
+  });
+};
+
+module.exports = {
+  setkv: setkv,
+  getkey: getkey
+};
