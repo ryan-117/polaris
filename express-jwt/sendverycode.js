8258dca1fcd96a6278649ea772e961b21034ff33
pengyurong <yurong.pyr@ncarzone.com>
2021-07-30 22:09:07 +0800

    发送验证码

---
+++ express-jwt/sendverycode.js
@@ -0,0 +1,12 @@
+// 发送验证码
+var sendmail = require('./sendmail');
+var sendphone = require('./sendphone');
+
+module.exports = {
+  mail: function (mail, verycode) {
+    return sendmail(mail, '西红柿APP的验证码', '您的验证码是' + verycode);
+  },
+  phone: function (phone, verycode) {
+    return sendphone(phone, '您的验证码是' + verycode);
+  }
+};
