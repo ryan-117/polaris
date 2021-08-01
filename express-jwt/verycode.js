d3e1c53d0a0155b64bee227bb3f7fa8e0a92d140
laststory117 <1172002409@qq.com>
2021-08-01 22:29:21 +0800

    es6替换

--- express-jwt/verycode.js
+++ express-jwt/verycode.js
@@ -1,21 +1,21 @@
 // 生成验证码
-var response = require('./response');
-var express = require('express');
-var route = express.Router();
-var sendverycode = require('./sendverycode');
-var common = require('./common');
-var config = require('./config');
-var expire = config.codeexpire;
-var redis = require('./redis');
+const response = require('./response');
+const express = require('express');
+const route = express.Router();
+const sendverycode = require('./sendverycode');
+const common = require('./common');
+const config = require('./config');
+const expire = config.codeexpire;
+const redis = require('./redis');
 
-route.post('/verycode', function (req, res) {
+route.post('/verycode', (req, res) => {
   //生成6位验证码
-  var vcode = Math.ceil(Math.random() * (1000000 - 100000) + 100000);
+  let vcode = Math.ceil(Math.random() * (1000000 - 100000) + 100000);
   console.log(vcode);
 
   if (req.body.type == 1) {
     //邮件
-    var mail = req.body.mail;
+    let mail = req.body.mail;
     if (common.ismail(mail) == false) {
       res.send(response.err('请输入正确的邮件地址'));
       return;
@@ -24,7 +24,7 @@ route.post('/verycode', function (req, res) {
     verify(mail);
   } else {
     //手机
-    var phone = req.body.phone;
+    const phone = req.body.phone;
     if (common.isphone(phone) == false) {
       res.send(response.err('请输入正确的手机号'));
       return;
@@ -33,7 +33,7 @@ route.post('/verycode', function (req, res) {
     verify(phone);
   }
 
-  function verify(key) {
+  let verify = key => {
     redis
       .getkey(key)
       .then(data => {
@@ -59,7 +59,7 @@ route.post('/verycode', function (req, res) {
           console.log('验证码发送失败');
         }
       });
-  }
+  };
 });
 
 module.exports = route;
