c8e0a4b0f793293d98c279bc6a02aedbc46bc3d7
laststory117 <1172002409@qq.com>
2021-08-09 23:14:45 +0800

    no message

--- express-jwt/config.js
+++ express-jwt/config.js
@@ -1,18 +1,18 @@
 // 配置文件：全局变量、jwt秘钥等
-CONST isthinkpad=true
-CONST mail = {
-    host: 'smtp.163.com',
-    user: 'xxxxx@163.com', // generated ethereal user
-    pass: 'xxxx', // generated ethereal password
-    from: '"Fred Foo" <xxxx@163.com>', // sender address
-    to: 'xxxx@qq.com,', // list of receivers
-}
+const isthinkpad = true;
+const mail = {
+  host: 'smtp.163.com',
+  user: 'xxxxx@163.com', // generated ethereal user
+  pass: 'xxxx', // generated ethereal password
+  from: '"Fred Foo" <xxxx@163.com>', // sender address
+  to: 'xxxx@qq.com,' // list of receivers
+};
 
 module.exports = {
-    codeexpire:180,//秒
-    jwtsecret:"jjjjjj",
-    md5secret:"jkkks934(EIURLOE(W)WF<{fs;f{{",
-    mongolink:isthinkpad?'mongodb://test:123456@192.168.99.100:27017/test':'mongodb://test:123456@localhost:27017/test',
-    redislink:isthinkpad?'192.168.99.100':'localhost',
-    mail:mail,
-}
+  codeexpire: 180, //秒
+  jwtsecret: 'jjjjjj',
+  md5secret: 'jkkks934(EIURLOE(W)WF<{fs;f{{',
+  mongolink: isthinkpad ? 'mongodb://test:123456@192.168.99.100:27017/test' : 'mongodb://test:123456@localhost:27017/test',
+  redislink: isthinkpad ? '192.168.99.100' : 'localhost',
+  mail: mail
+};
