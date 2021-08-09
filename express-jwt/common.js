95c2ed6b165456c8672a5cae160ebf059c22ddd7
laststory117 <1172002409@qq.com>
2021-08-09 23:16:36 +0800

    no message

--- express-jwt/common.js
+++ express-jwt/common.js
@@ -1,52 +1,40 @@
 // 通用方法：加密、token正确性检测，手机邮箱正则校检
-var config = require('./config')
-var jwt = require('jsonwebtoken')
-var md5 = require('md5')
+const config = require('./config');
+const jwt = require('jsonwebtoken');
+const md5 = require('md5');
 
-var signtoken = function (obj) {
-  return jwt.sign(obj, config.jwtsecret)
-}
+const signtoken = obj => jwt.sign(obj, config.jwtsecret);
 
-var verifytoken = function (token) {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, config.jwtsecret, function (err, decoded) {
+const verifytoken = token =>
+  new Promise((resolve, reject) => {
+    jwt.verify(token, config.jwtsecret, (err, decoded) => {
       if (err) {
-        resolve(null)
+        resolve(null);
+      } else {
+        resolve(decoded);
       }
-      else {
-        resolve(decoded)
-      }
-    })
-  })
-}
+    });
+  });
 
-var cmd5 = function (str) {
-  return md5(str + config.md5secret)
-}
+const cmd5 = str => md5(str + config.md5secret);
 
-var ismail = function (mail) {
-  var filter = /^[\w.\-]+@(?:[a-z0-9]+(?:-[a-z0-9]+)*\.)+[a-z]{2,3}$/
+const ismail = mail => {
+  const filter = /^[\w.\-]+@(?:[a-z0-9]+(?:-[a-z0-9]+)*\.)+[a-z]{2,3}$/;
   if (filter.test(mail)) {
-    return true
+    return true;
+  } else {
+    return false;
   }
-  else {
-    return false
-  }
-}
+};
 
-var isphone = function (phone) {
-  if (String(phone).length == 11) {
-    return true
-  }
-  else {
-    return false
-  }
-}
+const isphone = phone => {
+  return String(phone).length === 11;
+};
 
 module.exports = {
-  signtoken: signtoken,
-  verifytoken: verifytoken,
+  signtoken,
+  verifytoken,
   md5: cmd5,
-  ismail: ismail,
-  isphone: isphone
-}
+  ismail,
+  isphone
+};
