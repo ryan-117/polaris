1e4805bf5b37cd91a3050eeec8065a05231e51df
pengyr <pengyr@asiainfo.com>
2022-05-25 11:25:26 +0800

    修复安全监测问题

--- packages/express-jwt/mongoconn.js
+++
@@ -1,8 +0,0 @@
-// 数据库连接
-const config = require('./config');
-const mongoose = require('mongoose');
-
-mongoose.connect(config.mongolink, { autoIndex: false, useNewUrlParser: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, '数据库connection error:'));
