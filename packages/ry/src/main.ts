aada188f22bcc514711fdd217793a38d1fab5d3d
117 <1172002409@qq.com>
2022-06-08 17:56:02 +0800

    vite app

--- packages/ry/src/main.ts
+++ packages/ry/src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(ElementPlus).mount('#app')
